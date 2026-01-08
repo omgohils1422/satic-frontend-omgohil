c55b84e0018bc7d296871c97af23a332ad99122c
omgohils1422 <omgohil2236@gmail.com>
2026-01-08 16:30:39 +0530

    Add Hamburger Icon

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 document.querySelector(".primary-btn").innerText = "Send";
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+const links = navLinks.querySelectorAll("a");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+
+  // Disable background scroll
+  document.body.style.overflow =
+    navLinks.classList.contains("show") ? "hidden" : "auto";
+});
+
+// Close menu on link click
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+    document.body.style.overflow = "auto";
+  });
+});
